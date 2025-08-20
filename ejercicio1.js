81faf3d699db1fcf0e701478ef2ea9bef2130639
manvargs92 <manuel.vargas.garrido@outlook.com>
2025-08-20 13:50:22 -0600

    Ejercicio 1.

---
+++ ejercicio1.js
@@ -0,0 +1,5 @@
+var comidas = 2800 + 1300;
+var bebidas = 900 + 650;
+var totalCuenta = comidas + bebidas;
+
+alert("El total de comida es: $" + comidas + ", el total de bebidas es: $" + bebidas + ", el Gran Total es: $" + totalCuenta);
