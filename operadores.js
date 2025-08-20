f963035c7bb9b3e35a225d3b9822e81f8f15959f
manvargs92 <manuel.vargas.garrido@outlook.com>
2025-08-20 13:39:27 -0600

    Operadores en JavaScript.

---
+++ operadores.js
@@ -0,0 +1,51 @@
+//Operadores en JavaScript
+
+/* Operadores aritméticos:
+- Suma +
+- Resta -
+- Multiplicación *
+- División /
+- Módulo %
+- Incremento ++
+- Decremento --
+*/
+var suma = 35 + 3;
+console.log(suma);
+
+var resta = 20 - 4;
+console.log(resta);
+
+var multiplicación = 5 * 5;
+console.log(multiplicación);
+
+var division = 30 / 2;
+console.log(division);
+
+var resto = 60 % 2;
+console.log(resto);
+
+/* Operadores de comparación o relacionales:
+- Igualdad ==
+- Desigualdad (distinto) !=
+- Igualdad estricta ===
+- Desigualdad estricta !==
+- Mayor y menor que > <
+- Mayor o igual y menor o igual >= <=
+*/
+console.log(5 > 2);
+console.log(98 < 1);
+console.log(4 == 4);
+
+/* Operadores condicionales o lógicos
+- AND &&
+- OR ||
+- NOT !
+*/
+var edad = 18;
+var dni = !true; 
+
+console.log(edad >= 18 && dni == true);
+
+console.log(edad >= 18 || dni == true);
+
+console.log(dni);
